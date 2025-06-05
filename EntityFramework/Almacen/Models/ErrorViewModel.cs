43fa8424b5c683b2da795f00387a9e8bb91195bf
unknown <Hilary V>
2025-06-05 18:15:50 -0400

    Primer commit

---
+++ EntityFramework/Almacen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Almacen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
